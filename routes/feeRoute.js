a3e438126273ca0b4645e00a197b692aab0e234e
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-01-15 01:21:25 +0500

    fee status on dashboard

--- routes/feeRoute.js
+++ routes/feeRoute.js
@@ -3,13 +3,15 @@ const {
     getFee,
     storeFee,
     downloadPDF,
-    generateReport
+    generateReport,
+    getFeeByStudent
 } = require("../controllers/FeeController")
 const { requireLogin } = require("../middlewares/requireLogin");
 const { fee_validator } = require("../middlewares/feeValidator");
 
 router.get("/getFee",requireLogin,getFee)
 router.post("/storeFee",fee_validator,storeFee)
+router.get("/getFeeByStudent/:id",getFeeByStudent)
 router.get("/download",downloadPDF)
 router.post("/generate",generateReport)
 
