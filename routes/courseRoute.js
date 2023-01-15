cc3e533e4cc471fac8c03e5f4b1a9ef6dec725d7
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-01-15 23:48:25 +0500

    update

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -24,7 +24,7 @@ router.get("/get-courses", requireLogin, getCourses__controller);
 router.get("/get-course/:courseId", requireLogin, getOneCourse__controller)
 
 router.post('/delete-course',
-// requireLogin,adminAuthentication,
+requireLogin,adminAuthentication,
 deleteCourse__Controller)
 router.post('/edit-course',requireLogin,adminAuthentication,upload.single("img"),editCourse__Controller)
 
